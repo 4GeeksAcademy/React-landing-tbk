a8aa9c661667da6abccaac9bf4475c70a7117740
Tomás Beckmann <147322170+tomasbeckmann@users.noreply.github.com>
2023-12-12 22:02:00 +0000

    listo

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export const Card = (props) => {
     return (
-            <div className="col-md">
-                <div className="card">
-                    <img src={props.cardimg} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title"> {props.cardtitle} </h5>
-                        <p className="card-text">{props.cardtext}</p>
-                        <a href="#" className="btn btn-primary">{props.cardbutton}</a>
-                    </div>
+        <div className="col-md">
+            <div className="card mb-5">
+                <img src={props.cardimg} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title"> {props.cardtitle} </h5>
+                    <p className="card-text">{props.cardtext}</p>
+                    <a href="#" className="btn btn-primary">{props.cardbutton}</a>
                 </div>
             </div>
+        </div>
     );
 };
